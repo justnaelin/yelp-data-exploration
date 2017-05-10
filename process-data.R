d3b2041b931b870acb14057f13cac50e10b77793
clarissavazquez <cvasquez-ramo@csumb.edu>
2017-05-10 13:06:02 -0700

    Now able to read in large json files without crashing...sort of

--- process-data.R
+++ process-data.R
@@ -1,57 +1,14 @@
 library(rjson)
 
-# pre-process user data
-user_data = function(user_json_file) {
-  user_lines = readLines(user_json_file, n=25)
-  user_dat = lapply(user_lines, fromJSON)
-  user_dat = do.call("rbind", user_dat)
-  user_dat = data.frame(user_dat)
-  user_dat$yelping_since = unlist(user_dat$yelping_since)
-  user_dat$yelping_since = as.POSIXlt(user_dat$yelping_since, tz="US/Pacific", "%Y-%m-%d")
-  user_dat$user_id = unlist(user_dat$user_id)
-  user_dat$name = unlist(user_dat$name)
-  user_dat$review_count = unlist(user_dat$review_count)
-  user_dat$useful = unlist(user_dat$useful)
-  user_dat$funny = unlist(user_dat$funny)
-  user_dat$cool = unlist(user_dat$cool)
-  user_dat$fans = unlist(user_dat$fans)
-  user_dat$average_stars = unlist(user_dat$average_stars)
-  user_dat$compliment_hot = unlist(user_dat$compliment_hot)
-  user_dat$compliment_more = unlist(user_dat$compliment_more)
-  user_dat$compliment_profile = unlist(user_dat$compliment_profile)
-  user_dat$compliment_cute = unlist(user_dat$compliment_cute)
-  user_dat$compliment_list = unlist(user_dat$compliment_list)
-  user_dat$compliment_note = unlist(user_dat$compliment_note)
-  user_dat$compliment_photos = unlist(user_dat$compliment_photos)
-  user_dat$compliment_plain = unlist(user_dat$compliment_plain)
-  user_dat$compliment_cool = unlist(user_dat$compliment_cool)
-  user_dat$compliment_funny = unlist(user_dat$compliment_funny)
-  user_dat$compliment_writer = unlist(user_dat$compliment_writer)
-  
-  return (user_dat)
-}
-
 # pre-process business data
 business_data = function(business_json_file) {
-  business_lines = readLines(business_json_file, n=25)
-  business_dat = lapply(business_lines, fromJSON)
-  business_dat = do.call("rbind", business_dat)
-  business_dat = data.frame(business_dat)
-  business_dat$business_id = unlist(business_dat$business_id)
-  business_dat$name = unlist(business_dat$name)
-  business_dat$stars = unlist(business_dat$stars)
-  business_dat$review_count = unlist(business_dat$review_count)
-  
+  business_dat = stream_in(file(business_json_file), pagesize=10000)
   return (business_dat)
 }
 
 # pre-process checkin data
 checkin_data = function(checkin_json_file) {
-  checkin_lines = readLines(checkin_json_file, n=25)
-  checkin_dat = lapply(checkin_lines, fromJSON)
-  checkin_dat = do.call("rbind", checkin_dat)
-  checkin_dat = data.frame(checkin_dat)
-  checkin_dat$business_id = unlist(checkin_dat$business_id)
+  checkin_dat = stream_in(file(checkin_json_file), pagesize=10000)
   temp_times = list()
   for (i in 1:length(checkin_dat$time)) {
     temp = unlist(checkin_dat$time[i])
@@ -94,18 +51,7 @@ checkin_data = function(checkin_json_file) {
 
 # pre-process review data
 review_data = function(review_json_file) {
-  review_lines = readLines(review_json_file, n=25)
-  review_dat = lapply(review_lines, fromJSON)
-  review_dat = do.call("rbind", review_dat)
-  review_dat = data.frame(review_dat)
-  review_dat$user_id = unlist(review_dat$user_id)
-  review_dat$business_id = unlist(review_dat$business_id)
-  review_dat$stars = unlist(review_dat$stars)
-  review_dat$text = unlist(review_dat$text)
-  review_dat$useful = unlist(review_dat$useful)
-  review_dat$funny = unlist(review_dat$funny)
-  review_dat$cool = unlist(review_dat$cool)
-  review_dat$date = unlist(review_dat$date)
+  review_dat = stream_in(file(review_json_file), pagesize=10000)
   review_dat$date = as.POSIXlt(review_dat$date, tz="US/Pacific", "%Y-%m-%d")
 
   return (review_dat)
@@ -113,16 +59,17 @@ review_data = function(review_json_file) {
 
 # tip data
 tip_data = function(tip_json_file) {
-  tip_lines = readLines(tip_json_file, n=25)
-  tip_dat = lapply(tip_lines, fromJSON)
-  tip_dat = do.call("rbind", tip_dat)
-  tip_dat = data.frame(tip_dat)
-  tip_dat$text = unlist(tip_dat$text)
-  tip_dat$likes = unlist(tip_dat$likes)
-  tip_dat$business_id = unlist(tip_dat$business_id)
-  tip_dat$user_id = unlist(tip_dat$user_id)
-  tip_dat$date = unlist(tip_dat$date)
+  tip_dat = stream_in(file(tip_json_file), pagesize=10000)
   tip_dat$date = as.POSIXlt(tip_dat$date, tz="US/Pacific", "%Y-%m-%d")
   
   return (tip_dat)
 }
+
+# pre-process user data
+user_data = function(user_json_file) {
+  user_dat = stream_in(file(user_json_file), pagesize=10000)
+  user_dat$yelping_since = as.POSIXlt(user_dat$yelping_since, tz="US/Pacific", "%Y-%m-%d")
+  return (user_dat)
+}
+
+
