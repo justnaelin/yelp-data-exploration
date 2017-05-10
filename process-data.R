dace247b7f1c2bac0b516d4180e1fc8115163fae
ana <anaperez@csumb.edu>
2017-05-10 12:57:50 -0700

    fixed preprocessing of checkin data to obtain only the hour without mins

--- process-data.R
+++ process-data.R
@@ -58,30 +58,37 @@ checkin_data = function(checkin_json_file) {
     mon = grep("Mon", temp)
     mon_hours = temp[mon]
     mon_hours = sub("Mon-", "", mon_hours)
+    mon_hours = as.numeric(gsub("([0-9]+).*$", "\\1", mon_hours))
     
     tue = grep("Tue", temp)
     tue_hours = temp[tue]
     tue_hours = sub("Tue-", "", tue_hours)
+    tue_hours = as.numeric(gsub("([0-9]+).*$", "\\1", tue_hours))
     
     wed = grep("Wed", temp)
     wed_hours = temp[wed]
     wed_hours = sub("Wed-", "", wed_hours)
+    wed_hours = as.numeric(gsub("([0-9]+).*$", "\\1", wed_hours))
     
     thu = grep("Thu", temp)
     thu_hours = temp[thu]
     thu_hours = sub("Thu-", "", thu_hours)
+    thu_hours = as.numeric(gsub("([0-9]+).*$", "\\1", thu_hours))
     
     fri = grep("Fri", temp)
     fri_hours = temp[fri]
     fri_hours = sub("Fri-", "", fri_hours)
+    fri_hours = as.numeric(gsub("([0-9]+).*$", "\\1", fri_hours))
     
     sat = grep("Sat", temp)
     sat_hours = temp[sat]
     sat_hours = sub("Sat-", "", sat_hours)
+    sat_hours = as.numeric(gsub("([0-9]+).*$", "\\1", sat_hours))
     
     sun = grep("Sun", temp)
     sun_hours = temp[sun]
     sun_hours = sub("Sun-", "", sun_hours)
+    sun_hours = as.numeric(gsub("([0-9]+).*$", "\\1", sun_hours))
     
     temp_times[[length(temp_times) + 1]] = list(mon=mon_hours, tue=tue_hours, wed=wed_hours, 
                       thu=thu_hours, fri=fri_hours, sat=sat_hours, sun=sun_hours)
